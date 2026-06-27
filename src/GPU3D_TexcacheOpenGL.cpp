e78c108c38fc18546659c85a774a42d6572319e1
Isan440 <muhamadisan666@gmail.com>
2026-06-27 16:28:26 +0700

    Change texture path

--- src/GPU3D_TexcacheOpenGL.cpp
+++ src/GPU3D_TexcacheOpenGL.cpp
@@ -20,7 +20,7 @@ static bool initialized = false;
 
 if (!initialized)
 {
-    HDTexture::Initialize("/sdcard/Android/data/me.magnum.melonds.nightly.dev/files/textures");
+    HDTexture::Initialize(HDTexture::Initialize("/storage/emulated/0/melonDS");
     initialized = true;
 }
     glBindTexture(GL_TEXTURE_2D_ARRAY, handle);
